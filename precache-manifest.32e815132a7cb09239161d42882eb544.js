57570a78451a91117a72ad2d4d9f844dc88ecd62
Mallikarjun <mallikarjun@forktechnologies.in>
2019-04-05 17:16:46 +0530

    Updates

---
+++ precache-manifest.32e815132a7cb09239161d42882eb544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb97573012f02be72845",
+    "url": "/listDisplay/static/js/runtime~main.279bbf9b.js"
+  },
+  {
+    "revision": "0615ee6d8e48bedc36fe",
+    "url": "/listDisplay/static/js/main.c16a865d.chunk.js"
+  },
+  {
+    "revision": "1ec4d3fdfd9dfd0c9d0e",
+    "url": "/listDisplay/static/js/2.05e78aea.chunk.js"
+  },
+  {
+    "revision": "0615ee6d8e48bedc36fe",
+    "url": "/listDisplay/static/css/main.51e7cdc8.chunk.css"
+  },
+  {
+    "revision": "1ec4d3fdfd9dfd0c9d0e",
+    "url": "/listDisplay/static/css/2.48641a79.chunk.css"
+  },
+  {
+    "revision": "c356214bd2bf245e6b372915025d4c3d",
+    "url": "/listDisplay/index.html"
+  }
+];
